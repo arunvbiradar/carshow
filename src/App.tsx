a4d9aaa3998b5a55ece88dc18ec6c1c9a7068006
Arun Biradar <developer.avb@gmail.com>
2024-03-02 04:01:48 +0530

    update routing for ghpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Make from "./pages/Make";
 import NotFound from "./pages/NotFound";
@@ -7,14 +7,14 @@ import Model from "./pages/Model";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/make" element={<Make />} />
         <Route path="/model" element={<Model />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
